2c135d6fee9a674ea05cb3abe50092114362e3d5
D. Erik Goodman <goodman.d.erik@gmail.com>
2014-07-05 17:08:00 -0700

    potentiometer sends int to processing

--- Arduino/Control_panel/Control_panel.ino
+++ Arduino/Control_panel/Control_panel.ino
@@ -1,14 +1,29 @@
 // gather sensor data, sends to Processing
 
+int allVals[2];
+
+// sensors
+int pot; //0-1023
+int buttonLoc = 2;
+int button; // 0-1
+
+
 void setup() {
   // contact port
   Serial.begin(9600);
- 
+  pinMode(buttonLoc, INPUT);
 }
 
 void loop() {
-  // make contact
-  if (Serial.available() > 0) {
+  pot = analogRead(A0);
+  // map pot values to Processing values
+  pot = map(pot, 0, 1023, 0, 255);
+  allVals[0] = pot;
   
-  }  
+//  button = digitalRead(buttonLoc);
+//  allVals[1] = button;
+  // make contact
+  Serial.write(allVals[0]);
+  //Serial.write(allVals[1]);
+  delay(100);
 }  
